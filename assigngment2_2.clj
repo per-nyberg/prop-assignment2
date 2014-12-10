076802ea2e3f09f534cc4e78c9041abd406288dc
per-nyberg <per.nyberg0@gmail.com>
2014-12-10 17:13:49 +0100

    Create assigngment2_2.clj

---
+++ assigngment2_2.clj
@@ -0,0 +1,14 @@
+; ASSIGNMENT 2 SQL-like macro
+;
+;The second macro should implement an SQL-like syntax for searching in lists of maps, as illustrated
+;by the following example:
+;=> (def persons '({:id 1 :name "olle"} {:id 2 :name "anna"} {:id 3 :name
+;"isak"} {:id 4 :name "beatrice"}))
+;...
+;=> (select [:id :name] from persons where [:id > 2] orderby :name)
+;({:id 4 :name "beatrice"} {:id 3 :name "isak"})
+;The query format should be:
+;(select [columns] from #{table} where :column (op value) orderby :column)
+;where the condition operator 'op' is one of the following: = < > <>
+
+(defmacro SQL-syntax ([][]))
