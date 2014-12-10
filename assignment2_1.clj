7ff96dfe2c441dba16d0f1793994ab4dce091e5b
per-nyberg <per.nyberg0@gmail.com>
2014-12-10 17:12:26 +0100

    Update assignment2_1.clj

--- assignment2_1.clj
+++ assignment2_1.clj
@@ -16,15 +16,15 @@
 
 ;Pseudo code
 
-try (Socket s = new Socket()
-arg = println("123");
+;;;try (Socket s = new Socket()
+;;arg = println("123");
 
-macro(vector[s,new Socket()],arg){
-	if(arg == safe){ ;how to check if safe?
-		close s
-		return arg ;call arg and print "123"
-	}else if(arg caused exception){
-		close s
-		return exception
-	}
-}
+;macro(vector[s,new Socket()],arg){
+;	if(arg == safe){ ;how to check if safe?
+;		close s
+;		return arg ;call arg and print "123"
+;	}else if(arg caused exception){
+;		close s
+;		return exception
+;	}
+;}
