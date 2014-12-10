afde0594d456e845f1a462e00629ca199a6c91a8
per-nyberg <per.nyberg0@gmail.com>
2014-12-10 17:12:01 +0100

    Update assignment2_1.clj

--- assignment2_1.clj
+++ assignment2_1.clj
@@ -1,4 +1,16 @@
-
+; ASSIGNMENT 2 Safe macro
+;
+;You should define a macro 'safe' with two arguments. The first argument should be a vector with two
+;elements, one variable and one value (corresponding to 's' and 'new Socket()' in the example above).
+;The second argument should be a form (expression) that should be evaluated. The macro 'safe'
+;should either return the return value of the second argument or an exception. In both cases the
+;variable should be closed (if it is an instance of java.io.Closeable; hint use 'instance?'). It should be
+;possible to use the variable inside the form (expression) as in the following example:
+;=> (import java.io.FileReader java.io.File)
+;
+;=> (safe [s (new FileReader (new File "file.txt"))] (.read s))
+;104
+;The return value 104 is the first byte in the file "file.txt".
 
 (defmacro safe ([][]))
 
